d0b16716b29e82bde31d997c513de903ba6d2668
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-06-09 17:16:00 +0530

    update

--- server/routes/games.js
+++ server/routes/games.js
@@ -1,4 +1,5 @@
-const games = require('../services/games')
+const games = require('../services/games');
+const { addGame, deleteGame, addAdUnit, delete_ad_unit } = require('../services/new');
 const router = require('express').Router();
 const get_revenue = require("../services/revenue.js")
 
@@ -7,5 +8,12 @@ router.put('/:devId', games.addGame);
 router.delete('/:devId/:gameId', games.deleteGames);
 
 router.post("/revenue", get_revenue);
+
+router.post("/addGame", addGame)
+router.delete("/deleteGame", deleteGame)
+
+// addAdUnit, delete_ad_unit
+router.post("/addAdUnit", addAdUnit)
+router.delete("/deleteAdUnit", delete_ad_unit)
 
 module.exports = router;
---
+++ server/services/new.js
@@ -0,0 +1,107 @@
+const collectionList = require("../fb/collection");
+
+async function addGame(req, res) {
+  try {
+    const { user_id, game_name, game_icon, platform } = req.body;
+    const data = {
+      game_name,
+      game_icon,
+      published_date: new Date(),
+      platform,
+      rank: 0,
+      status: "pending",
+      ad_cost_rate: 0,
+      ad_units: [],
+    };
+
+     const GameRef = await collectionList.gameCollection.add(data);
+     const game_id = GameRef.id;
+
+     const userRef = await collectionList.developerCollection.doc(user_id).get();
+     const game_list = userRef.data().games;
+     game_list.push(game_id);
+     await collectionList.developerCollection.doc(user_id).update({
+      games: game_list,
+    });
+
+    console.log(game_id);
+    res.status(201).send("Game has been created successfully!");
+ // res.send(game_list);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+}
+
+async function deleteGame(req,res) {
+    const { user_id, game_id } = req.body;
+
+   try {
+    const GameRef = await collectionList.gameCollection.doc(game_id).delete();
+    const UserRef = await collectionList.developerCollection.doc(user_id).get();
+    const game_list = UserRef.data().games;
+    game_list.splice(game_list.indexOf(game_id),1)
+    await collectionList.developerCollection.doc(user_id).update({
+        games: game_list,
+      }); 
+      res.status(204).send("Game has been deleted successfully!");
+
+   } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+   }
+}
+
+async function addAdUnit(req,res) {
+   
+    const {ad_unit_name,ad_unit_type, game_id  } = req.body;
+    const data = {
+        ad_unit_name,
+        ad_unit_type,
+        no_of_req_ad_daily: 0,
+        no_of_req_ad_monthly: 0,
+        created_date: new Date(),
+        total_daily_view_count: 0,
+        total_weekly_view_count: 0,
+        total_monthly_view_count: 0,
+      }
+
+      try {
+        const AdUnitRef = await collectionList.adUnitCollection.add(data);
+        const ad_unit_id = AdUnitRef.id;
+        const GameRef = await collectionList.gameCollection.doc(game_id).get();
+        const ad_unit_list = GameRef.data().ad_units;
+        ad_unit_list.push(ad_unit_id);
+        const UpdateGameRef = await collectionList.gameCollection.doc(game_id).update({
+            ad_units: ad_unit_list,
+        });
+        console.log(ad_unit_id);
+        res.status(204).send("Ad Unit has been added successfully!");
+
+        
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error");
+      }
+}
+
+async function delete_ad_unit(req,res) {
+    const { game_id, ad_unit_id } = req.body;
+
+    try {
+        const AdUnitRef = await collectionList.adUnitCollection.doc(ad_unit_id).delete();
+        const GameRef = await collectionList.gameCollection.doc(game_id).get();
+        const ad_unit_list = GameRef.data().ad_units;
+        ad_unit_list.splice(ad_unit_list.indexOf(ad_unit_id),1)
+        await collectionList.gameCollection.doc(game_id).update({
+            ad_units: ad_unit_list,
+          }); 
+          res.status(204).send("Ad Unit has been deleted successfully!");
+    
+       } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error");
+       }
+}
+
+module.exports = {addGame, deleteGame, addAdUnit, delete_ad_unit}
